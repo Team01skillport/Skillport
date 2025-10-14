f7cd49b7333e489433b2763d732e543452a0c454
masato-1919 <ookuramasato.halstudent@gmail.com>
2025-10-14 17:48:53 +0900

    講義動画テーブルまで作った

--- skillPort/database/db.sql
+++ skillPort/database/db.sql
@@ -1,6 +1,6 @@
 -- ユーザー情報テーブル作成
 CREATE TABLE user_tbl(
-id INT(10),
+id CHAR(10),
 user_name CHAR(16),
 first_name CHAR(8),
 last_name CHAR(8),
@@ -22,7 +22,7 @@ PRIMARY KEY(id)
 
 -- 支払い情報テーブル作成
 CREATE TABLE payment_tbl(
-    user_id INT(10),
+    user_id CHAR(10),
     card_num CHAR(16),
     card_name CHAR(16),
     card_expiration CHAR(4),
@@ -41,7 +41,7 @@ CREATE TABLE payment_tbl(
 
 --メンバーシップ情報テーブル
 CREATE TABLE membership_tbl(
-    user_id INT(10),
+    user_id CHAR(10),
     join_date DATE,
     renewal_date DATE,
     payment_status CHAR(16),
@@ -53,7 +53,7 @@ CREATE TABLE membership_tbl(
 
 --商品情報テーブル
 CREATE TABLE product_tbl(
-    id INT(32),
+    id CHAR(32),
     favorites INT(32),
     product_view_flag TINYINT(1),
     user_id	INT (10),	
@@ -62,7 +62,7 @@ CREATE TABLE product_tbl(
 
 --出品情報テーブル
 CREATE TABLE listing_tbl(
-    product_id	INT(64),
+    product_id	CHAR(64),
     product_image CHAR(128),		
     product_name INT(128),		
     product_price INT(7),
@@ -71,8 +71,8 @@ CREATE TABLE listing_tbl(
     product_condition CHAR(8),		
     product_description	VARCHAR(128),		
     listing_status TINYINT(1),						
-    listing_date DATE(8),		
-    sales_status CHAR(8),		
+    listing_date DATE,		
+    sales_status CHAR,		
     update_date	DATETIME,		
     product_upload_user	 INT(10),
     PRIMARY KEY(product_id,product_upload_user)
@@ -81,8 +81,8 @@ CREATE TABLE listing_tbl(
 --取引情報テーブル
 CREATE TABLE market_order_tbl(
     id						
-    purchaser_id INT(32),					
-    seller_id INT (10),					
+    purchaser_id CHAR(32),					
+    seller_id CHAR (10),					
     transaction_status CHAR(8),						
     transaction_startdate CHAR(8),				
     transaction_completeddate CHAR(8),    					
@@ -99,7 +99,7 @@ CREATE TABLE market_order_tbl(
 
 --取引メッセージテーブル
 CREATE TABLE order_message_tbl(
-    id INT(128),				
+    id CHAR(128),				
     order_message_date DATETIME					
     order_message_user_id INT(10),				
     transaction_id INT(32),		
@@ -108,4 +108,54 @@ CREATE TABLE order_message_tbl(
 );
 
 --投稿フィードアップロードテーブル
-CREATE TABLE post_tbl
+CREATE TABLE post_tbl(
+    id	CHAR(128),					
+    user_id CHAR(128),				
+    post_date DATETIME,			
+    post_text VARCHAR(128),			
+    post_media CHAR(128),		
+    post_update_date DATETIME					
+    post_report_flag TINYINT(1),				
+    post_status	TINYINT(1),
+    PRIMARY KEY(id)			
+);
+
+--投稿フィードいいねテーブル
+CREATE TABLE feed_like_tbl(
+    id INT(10),
+    post_id INT (10),
+    user_id INT (10),
+    like_time DATE,
+    PRIMARY KEY(id)
+);
+
+--投稿フィードコメントテーブル
+CREATE TABLE feed_comment_tbl(
+    id	CHAR(10),					
+    user_id	CHAR(10),					
+    post_id	CHAR(10),			
+    post VARCHAR(128),			
+    comment_date DATE,					
+    father_comment_id INT(10),
+    PRIMARY KEY(id)					
+);
+
+--講義動画テーブル
+CREATE TABLE video_tbl(
+    id CHAR(64),					
+    video_title CHAR(32),						
+    video_length INT(5),	
+    video_uploader_id INT(10),					
+    video_upload_date DATE,				
+    video_description_section VARCHAR(256),			
+    video_public_status TINYINT(1),		
+    video_category CHAR(16),						
+    video_tag CHAR(16)		
+    video_report_flag TINYINT(1),						
+    video_popularity_index FLOAT				
+    view_count INT (),	
+    like_count INT(),				
+    comment_count INT(),					
+    file_path VARCHAR(255),
+    PRIMARY KEY(id,video_uploader_id)
+);
