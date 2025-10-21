4fde9af117e2069d5c4ede9a275e5833e73488f4
Jeremiah Isaiah <55800485+JIsaiah@users.noreply.github.com>
2025-10-21 17:29:40 +0900

    register.cssの更新。まだできてないからなおしてほしい

--- skillPort/app/views/search.py
+++ skillPort/app/views/search.py
@@ -5,10 +5,5 @@ search_bp = Blueprint('search', __name__, url_prefix='/search')
 
 @search_bp.route('/header_search', methods=["GET"])
 def header_search():
-    errtbl = {}
     search_word = request.args.get("headersearch")
-    if not search_word:
-        errtbl["empty"] = "検索する文字を入力をしてください"
-        return render_template('/')
-    print(search_word)
     return render_template('search/header_search_result.html', search_word=search_word)
