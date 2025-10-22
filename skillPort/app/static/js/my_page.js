b5126dbf72978bca3e1b8b66e17a5c0c1051eebb
Jeremiah Isaiah <55800485+JIsaiah@users.noreply.github.com>
2025-10-22 10:48:59 +0900

    my_page.jsの作成、メニューボタンを押すとページが遷移する準備

---
+++ skillPort/app/static/js/my_page.js
@@ -0,0 +1,18 @@
+$(".menu_link").on("click", function (e) {
+  console.log("CLICK");
+  e.preventDefault();
+  const url = $(this).data("target");
+  console.log(url);
+  $.ajax({
+    url: url,
+    dataType: "html",
+    method: "GET",
+  })
+    .done(function (response) {
+      $("#main-content").html(response);
+    })
+    .fail(function () {
+      console.log("FAILED");
+    });
+  // .always(function () {});
+});
--- skillPort/app/views/my_page.py
+++ skillPort/app/views/my_page.py
@@ -5,9 +5,12 @@ my_page_bp = Blueprint('my_page', __name__, url_prefix='/my_page')
 
 @my_page_bp.route('/top', methods=["GET"])
 def my_page_top():
-    return render_template('my_page/my_page_top.html')
+    return render_template('my_page/mp_menu.html')
 
-@my_page_bp.route('/history', methods=["GET"])
+@my_page_bp.route('/view_history', methods=["GET"])
 def my_page_history():
-    return render_template('my_page/my_page_history.html')
+    return render_template('my_page/mp_view_history.html')
 
+@my_page_bp.route('/bank_account_register', methods=["GET"])
+def bank_account_register():
+    return render_template('my_page/mp_bank_account_register.html')
