6f3a12a708c4c483a48ef741ea83086dc2bc89ad
Jeremiah Isaiah <55800485+JIsaiah@users.noreply.github.com>
2025-10-22 10:18:02 +0900

    my_pageのメニュー作成

--- skillPort/app/views/account.py
+++ skillPort/app/views/account.py
@@ -4,11 +4,15 @@ from flask import Blueprint, render_template, request, make_response, session
 account_bp = Blueprint('account', __name__, url_prefix='/account')
 
 # /auth/loginにアクセスされた際の処理を記述
-@account_bp.route('/account/register', methods=["GET"])
+@account_bp.route('/register', methods=["GET"])
 def register_form():
     
     return render_template('account/register.html')
 
-@account_bp.route('/account/profile', methods=["GET"])
+@account_bp.route('/profile', methods=["GET"])
 def user_profile():
     return render_template('account/profile.html')
+
+@account_bp.route('/my_page_top', methods=["GET"])
+def my_page_top():
+    return render_template('my_page/mp_top.html')
