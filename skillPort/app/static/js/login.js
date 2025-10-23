cc8cb84231eac32d39afab9c48a117d125c63a3e
Jeremiah Isaiah <55800485+JIsaiah@users.noreply.github.com>
2025-10-23 13:13:49 +0900

    loginページで文字列入力はされているかが確認できる

--- skillPort/app/static/js/login.js
+++ skillPort/app/static/js/login.js
@@ -1,5 +1,20 @@
 console.log("JS LOADED");
 
-$(".form-actions").on("submit", function (response) {
-  let username = $("#userId");
+$(".login-form").on("submit", function (response) {
+  console.log("CLICK");
+
+  let username = $("#userId").val();
+  let password = $("#password").val();
+  if (username === "") {
+    response.preventDefault();
+    console.log("ENTER USERNAME");
+
+    $("#userId").attr("placeholder", "ユーザー名を入力してください");
+  }
+  if (password === "") {
+    response.preventDefault();
+    console.log("ENTER PASSWORD");
+
+    $("#password").attr("placeholder", "パスワードを入力してください");
+  }
 });
