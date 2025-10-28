10f300b1418e774eae7919f2ba5487984404b9c0
slbzbb <slbzbb0322@gmail.com>
2025-10-28 17:14:13 +0900

    app.py作成

---
+++ skillPort/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+import mysql.connector
+import math
+import json
+import os
+
+# (Blueprint)
+from app.views.auth import auth_bp
+from app.views.market import market_bp 
+from app.views.community import community_bp
+from app.views.category import category_bp
+from app.views.search import search_bp
+from app.views.account import account_bp 
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key' 
+
+app.register_blueprint(auth_bp)
+app.register_blueprint(market_bp)
+app.register_blueprint(community_bp)
+app.register_blueprint(category_bp)
+app.register_blueprint(search_bp)
+app.register_blueprint(account_bp)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host = "localhost", port=5000)
