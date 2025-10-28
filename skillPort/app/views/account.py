7f0c0ff7cc6fbf2b72bd95b43d3ad21284a31f5d
Jeremiah Isaiah <55800485+JIsaiah@users.noreply.github.com>
2025-10-28 22:42:21 +0900

    account.pyの新規登録処理

--- skillPort/app/views/account.py
+++ skillPort/app/views/account.py
@@ -1,24 +1,30 @@
 from flask import Blueprint, render_template, request, make_response, session
+import mysql.connector
 
-# Blueprintオブジェクトを作成
 account_bp = Blueprint('account', __name__, url_prefix='/account')
 
-# /auth/loginにアクセスされた際の処理を記述
-@account_bp.route('/register', methods=["GET"])
+@account_bp.route('/register_userlogin', methods=["GET"])
 def register_form():
     
     return render_template('account/account_register.html')
 
-@account_bp.route('/register_userlogin', methods=["POST"])
+@account_bp.route('/register_userinfo', methods=["POST"])
 def register_userlogin():
     username = request.form.get("username")
     password = request.form.get("password")
+    print(username)
+    print(password)
     return render_template('account/user_register.html', username=username, password=password)
 
-@account_bp.route('/register_userinfo', methods=["POST"])
+@account_bp.route('/register_success', methods=["POST"])
 def register_userinfo():
-    
-    sql = "INSERT INTO user_tbl (user_name, first_name, last_name, first_name_katakana, last_name_katakana, tel_no, zip_code, prefecture, address1, address2, address3, birthday, mail, password, introduction) VALUES ();"
+    con = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "",
+    db = "skillport_db"
+    )  
+    sql = "INSERT INTO user_tbl (user_name, first_name) VALUES ();"
     return render_template('account/profile.html')
 
 @account_bp.route('/profile', methods=["GET"])
