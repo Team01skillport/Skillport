25ccbb7ab0a5a08cefd623cea3573adb51824116
Masato-Okura <ookuramasato.halstudent@gmail.com>
2025-10-29 16:01:39 +0900

    ...

--- skillPort/test.py
+++ skillPort/test.py
@@ -1,16 +1,26 @@
-from flask import Flask, rendeer_template, request
+from flask import Flask, render_template
 import mysql.connector
-import math
-
 
 app = Flask(__name__)
 
-def connect_db():
-    con = mysql.connector.connect(
-        host = 'localhost',
-        user = 'root',
-        passwd = '',
-        db = 'oppai',
-
+# MySQL接続関数
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",       # XAMPPのMySQLサーバー
+        user="root",            # デフォルトユーザー
+        password="",            # パスワード未設定なら空欄
+        database="video_db"     # 作成済みのデータベース名
     )
-    return con
+
+@app.route('/lecture')
+def index():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, file_name, drive_file_id, upload_date FROM videos")
+    videos = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('lecture_video.html', videos=videos)
+
+if __name__ == '__main__':
+    app.run(debug=True)
