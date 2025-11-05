107265fe9b2df1c30026e591a6834dc56595d6da
Jeremiah Isaiah <55800485+JIsaiah@users.noreply.github.com>
2025-11-05 09:40:42 +0900

    毎回db.pyをいじらなくてよくした

--- skillPort/app/db.py
+++ skillPort/app/db.py
@@ -1,12 +1,21 @@
 from flask import Blueprint, render_template, request, make_response
 import mysql.connector
 def connect_db():
-    con = mysql.connector.connect(
-    host="localhost",
-    user = "root",
-    passwd = "",
-    db = "skillport_db"
-    )
+    try:
+        con = mysql.connector.connect(
+        host="localhost",
+        user = "root",
+        passwd = "",
+        db = "skillport_db"
+        )
+    except:
+        con = mysql.connector.connect(
+        host="localhost",
+        port = 8889,
+        user = "root",
+        passwd = "root",
+        db = "skillport_db"
+        )
     return con
     
 def fetch_query(sql,fetch_one=False):
