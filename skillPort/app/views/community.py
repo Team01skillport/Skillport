29da79f382b4d526573dc4d93998c2110af7f4aa
Jeremiah Isaiah <55800485+JIsaiah@users.noreply.github.com>
2025-11-05 13:05:12 +0900

    コミュニティページに各ユーザーの投稿が出るようにした

--- skillPort/app/views/community.py
+++ skillPort/app/views/community.py
@@ -5,4 +5,9 @@ community_bp = Blueprint('community', __name__, url_prefix='/community')
 
 @community_bp.route('/community/top', methods=["GET"])
 def community_top():
-    return render_template('community/community.html')
+    sql = "select p.*, u.id AS user_id, u.user_name, u.profile_icon from post_tbl p INNER JOIN user_tbl u ON p.user_id = u.id ORDER BY post_update_date ASC;"
+    all_posts = fetch_query(sql)
+    
+    if 'user_id' in session:
+        user_id = session['user_id']
+    return render_template('community/community.html', all_posts=all_posts, user_id=user_id)
