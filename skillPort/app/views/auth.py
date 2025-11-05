51ff68f41355af950911cd60bae499710b4f4210
Jeremiah Isaiah <55800485+JIsaiah@users.noreply.github.com>
2025-11-05 15:04:33 +0900

    profileページでアイコンの制御

--- skillPort/app/views/auth.py
+++ skillPort/app/views/auth.py
@@ -33,7 +33,11 @@ def view_profile():
         user_id = session['user_id']
         sql = "SELECT * FROM user_tbl WHERE id = '"+str(user_id)+"';"
         user_info = fetch_query(sql, params=None, fetch_one=True)
-        
+        user_icon = user_info['profile_icon']
+        if user_icon == None:
+            user_icon = "/icons/default_icon.png"
+        print(user_icon)
+
         try:
             introduction = user_info['introduction']
         except:
@@ -67,8 +71,7 @@ def view_profile():
         # ユーザーがアップした投稿を表示する
         sql = "SELECT p.* FROM post_tbl p INNER JOIN user_tbl u ON p.user_id = u.id WHERE u.id = '"+str(user_id)+"';"
         user_posts = fetch_query(sql)
-        print(user_posts)
-        return render_template('profile/profile.html', user_name=session['user_name'], introduction=introduction, tag=tag, rating_stars=rating_stars, user_videos=user_videos, user_posts=user_posts, novid_msg="このユーザーは動画をアップしていません", nopost_msg="このユーザーは投稿をアップしていません")
+        return render_template('profile/profile.html', user_name=session['user_name'], user_info=user_info, user_icon=user_icon, introduction=introduction, tag=tag, rating_stars=rating_stars, user_videos=user_videos, user_posts=user_posts, novid_msg="このユーザーは動画をアップしていません", nopost_msg="このユーザーは投稿をアップしていません")
     else:
         return redirect(url_for('auth.login'))
 
