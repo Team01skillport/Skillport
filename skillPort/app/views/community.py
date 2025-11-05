0c709a6071c24750309b1f8ac8894a8845637ea2
Jeremiah Isaiah <55800485+JIsaiah@users.noreply.github.com>
2025-11-05 15:15:26 +0900

    コミュニティページのバグ修正

--- skillPort/app/views/community.py
+++ skillPort/app/views/community.py
@@ -8,6 +8,7 @@ def community_top():
     sql = "select p.*, u.id AS user_id, u.user_name, u.profile_icon from post_tbl p INNER JOIN user_tbl u ON p.user_id = u.id ORDER BY post_update_date ASC;"
     all_posts = fetch_query(sql)
     
+    sql = "select * FROM video_tbl ORDER"
     if 'user_id' in session:
         user_id = session['user_id']
     return render_template('community/community.html', all_posts=all_posts, user_id=user_id)
