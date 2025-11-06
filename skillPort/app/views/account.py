0f4d87f4bbf2e25886bafe15aa7f99f5c3c4e3db
Jeremiah Isaiah <55800485+JIsaiah@users.noreply.github.com>
2025-11-06 13:33:31 +0900

    アカウント情報編集機能実装中

--- skillPort/app/views/account.py
+++ skillPort/app/views/account.py
@@ -45,7 +45,32 @@ def user_register_complete():
 def edit_profile(user_name):
     sql = "SELECT * FROM user_tbl WHERE user_name = '"+str(user_name)+"';"
     user_info = fetch_query(sql, params=None, fetch_one=True)
-    return render_template('profile/profile_edit.html', info=user_info)
+    return render_template('profile/profile_edit.html', info=user_info, user_name=user_name)
+
+@account_bp.route('/edit_profile/<user_name>/success', methods=["POST"])
+def edit_profile_success(user_name):
+
+    mail = request.form.get("email")
+    tel_no = request.form.get("tel")
+    last_name = request.form.get("last_name")
+    first_name = request.form.get("first_name")
+    last_name_furigana = request.form.get("last_name_furigana")
+    first_name_furigana = request.form.get("first_name_furigana")
+    birthday = request.form.get("birthday")
+    zip_code = request.form.get("zip_code")
+    prefecture = request.form.get("prefecture")
+    address1 = request.form.get("address1")
+    address2 = request.form.get("address2")
+    new_profile_icon = request.form.get("profile_icon")
+    introduction = request.form.get("introduction")
+    
+    sql = "INSERT INTO user_tbl (user_name, first_name, last_name, first_name_katakana, last_name_katakana, tel_no, zip_code, prefecture, address1, address2, birthday, gender, mail, password, introduction, profile_icon) VALUES ('"+username+"', '"+first_name+"', '"+last_name+"', '"+first_name_furigana+"', '"+last_name_furigana+"', '"+tel_no+"', '"+zip_code+"', '"+prefecture+"', '"+address1+"', '"+address2+"', '"+birthday+"', '"+gender+"', '"+mail+"', '"+password+"', '"+introduction+"', '"+new_profile_icon+"');"
+    update_user_info = create_user(sql)
+    print(update_user_info)
+
+    sql = "SELECT * FROM user_tbl WHERE user_name = '"+str(user_name)+"';"
+    user_info = fetch_query(sql, params=None, fetch_one=True)
+    return render_template('profile/profile_edit.html')
 
 @account_bp.route('/my_page_top', methods=["GET"])
 def my_page_top():
