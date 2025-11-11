251937b4cf74e0603844e2ac55e2c51698fdee7b
Jeremiah Isaiah <55800485+JIsaiah@users.noreply.github.com>
2025-11-11 16:39:08 +0900

    講義動画の画像表示機能の追加

--- skillPort/app/views/category.py
+++ skillPort/app/views/category.py
@@ -9,6 +9,7 @@ def category_list():
     print(cat_name)
     
     sql = "SELECT * FROM video_tbl WHERE video_category = '"+cat_name+"';"
-    category_videos = fetch_query(sql)
+    category_videos = fetch_query(sql, params=None, fetch_one=False)
     print(category_videos)
+
     return render_template('lesson_video/lecture_video_list.html', cat_name=cat_name, category_videos=category_videos)
