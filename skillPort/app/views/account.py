bdc4e90db9eff6088ac4cb178f26ea2d8d622782
Jeremiah Isaiah <55800485+JIsaiah@users.noreply.github.com>
2025-11-20 12:13:53 +0900

    プロフィールアイコンを更新する際のバグを修正

--- skillPort/app/views/account.py
+++ skillPort/app/views/account.py
@@ -59,13 +59,16 @@ def edit_profile(user_name):
 
 @account_bp.route('/edit_profile/<user_name>/success', methods=["POST"])
 def edit_profile_success(user_name):
+    icon_sql = "SELECT profile_icon FROM user_tbl WHERE user_name = %s"
+    icon_data = fetch_query(icon_sql, (user_name,), True)
+    
     user_tags = request.form.get("tags")
     mail = request.form.get("email")
     tel_no = request.form.get("phone_num")
     last_name = request.form.get("last_name")
     first_name = request.form.get("first_name")
-    last_name_furigana = request.form.get("last_name_furigana")
-    first_name_furigana = request.form.get("first_name_furigana")
+    last_name_katakana = request.form.get("last_name_katakana")
+    first_name_katakana = request.form.get("first_name_katakana")
     year = request.form.get("year")
     month = request.form.get("month")
     day = request.form.get("day")
@@ -75,10 +78,9 @@ def edit_profile_success(user_name):
     address2 = request.form.get("address2")
     new_profile_icon = request.files.get('profile_icon')
     introduction = request.form.get("self_introduction")
-    print(new_profile_icon)
     date_string = f"{year}-{month}-{day}"
     if not new_profile_icon:
-        icon_db_path = "/icons/default_icon.png"
+        icon_db_path = icon_data
     else:
         filename = secure_filename(new_profile_icon.filename)
         local_icon_path = os.path.join(current_app.root_path, 'static', 'media', 'user', 'icons')
@@ -92,8 +94,8 @@ def edit_profile_success(user_name):
     UPDATE user_tbl SET
     first_name = '{first_name}',
     last_name = '{last_name}',
-    first_name_katakana = '{first_name_furigana}',
-    last_name_katakana = '{last_name_furigana}',
+    first_name_katakana = '{first_name_katakana}',
+    last_name_katakana = '{last_name_katakana}',
     tel_no = '{tel_no}',
     zip_code = '{zip_code}',
     prefecture = '{prefecture}',
